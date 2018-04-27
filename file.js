b7c4d6637a0d19699331776214c6393f0cded677
Agboola Emmanuel Olorunleke <olorunlexe@gmail.com>
2018-04-27 19:44:40 +0100

    simple examination application functioality
    A sample made with Javascript

---
+++ file.js
@@ -0,0 +1,47 @@
+
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+var questions = [
+    ["What is my name?", "Femi","Tolu","Jide","B"],
+    ["Where do I stay?", "Futa-South","Futa-North","Futa-West","A"],
+    ["What is my Department?", "AGY","MEE","EEE","B"],
+    ["Where do I fellowship", "Caccf","Rcf","CCF-Nifes", "A"]
+  ];
+function _(x){
+    return document.getElementById(x);
+}
+function renderQuestion(){
+ test= _("test");
+ if(pos >= questions.length){
+     test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+     _("test_status").innerHTML = "Test Completed";
+     pos = 0;
+     correct =0;
+     return false;
+ }
+ _("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+question = questions[pos][0];
+chA = questions[pos][1];
+chB = questions[pos][2];
+chC = questions[pos][3];
+test.innerHTML = "<h3>"+question+"</h3>";
+test.innerHTML += "<input type='radio' name='choices' value='A'> " +chA+"<br>";
+test.innerHTML += "<input type='radio' name='choices' value='B'> " +chB+"<br>";
+test.innerHTML += "<input type='radio' name='choices' value='C'> " +chC+"<br><br>"; 
+test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+function checkAnswer() {
+  choices = document.getElementsByName("choices");
+for(var i=0; i<choices.length;i++){
+    if(choices[i].checked){
+        choice = choices[i].value;
+    }
+   
+    
+}
+     if (choice == questions[pos][4]){
+        correct++;
+    }
+    pos++;
+    renderQuestion();
+}
+window.addEventListener('load',renderQuestion,false);   
